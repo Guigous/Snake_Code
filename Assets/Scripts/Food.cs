22dd1ff9c78034564bf22c63b3b820f64430bd60
Guigous <gui_1810@hotmail.com>
2021-12-01 20:04:55 -0300

    Todo O codigo comentado

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -6,20 +6,20 @@ public class Food : MonoBehaviour
 
     private void Start()
     {
-        //
+        //metodo de randomizacao
         RandomizePosition();
     }
 
     public void RandomizePosition()
     {
-        //
+        //limites da grid
         Bounds bounds = this.gridArea.bounds;
 
-        //
+        //valores de limites da grid
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
 
-        // 
+        // arredodamento para que a fruta apareca em espacos fisicos
         x = Mathf.Round(x);
         y = Mathf.Round(y);
 
@@ -28,7 +28,7 @@ public class Food : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //
+        //uso do metodo para randomizar a localizacao da fruta quando o player passa por ela
         RandomizePosition();
     }
 
